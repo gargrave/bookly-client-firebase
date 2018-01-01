4b31e764c24efa77010e87d1629adf3a46bb152b
Gabe Hargrave <ghargrave@workwithopal.com>
2018-01-01 10:30:52 -0800

    Clean up mock data a bit

--- src/utils/mocks/data/authors-mock-data.js
+++ src/utils/mocks/data/authors-mock-data.js
@@ -1,30 +1,30 @@
 export const authorsMockData = [
   {
     id: 'oiw223h3qndlksadffl3ksdf',
-    firstName: 'Aldous',
-    lastName: 'Huxley',
     created: '2017-07-01T01:16:43.881Z',
     updated: '2017-07-01T01:16:43.881Z',
+    firstName: 'Aldous',
+    lastName: 'Huxley',
   },
   {
     id: '9328fhs938hfdsdblkhf98',
-    firstName: 'Kurt',
-    lastName: 'Vonnegut',
     created: '2017-07-06T22:02:11.504Z',
     updated: '2017-07-06T22:02:11.504Z',
+    firstName: 'Kurt',
+    lastName: 'Vonnegut',
   },
   {
     id: '92nd9ab3kjasegfhasf783fu',
-    firstName: 'George R. R.',
-    lastName: 'Martin',
     created: '2017-08-17T20:47:35.232Z',
     updated: '2017-08-17T20:47:35.232Z',
+    firstName: 'George R. R.',
+    lastName: 'Martin',
   },
   {
     id: '98fn98w3ucbe8c7b276v',
-    firstName: 'Tom',
-    lastName: 'Robbins',
     created: '2017-07-06T22:02:17.051Z',
     updated: '2017-08-17T21:27:35.885Z',
+    firstName: 'Tom',
+    lastName: 'Robbins',
   },
 ];
--- src/utils/mocks/data/users-mock-data.js
+++ src/utils/mocks/data/users-mock-data.js
@@ -1,9 +1,9 @@
 export const usersMockData = [
   {
     uid: 'h6EfCE6MrJ5IVf2ay3QsPdX5Jd23',
-    email: 'mockuser1@gmail.com',
     registered: '2017-06-30T19:52:54.644Z',
     lastLogin: '2017-07-01T01:11:03.942Z',
+    email: 'mockuser1@gmail.com',
     verified: true,
   },
 ];
