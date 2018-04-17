24aa6482b701d4148a6b1b5740ce0b92afc39d73
Gabe Hargrave <gkhargrave@gmail.com>
2018-04-16 21:31:25 -0700

    Add Profile mock data

---
+++ src/globals/mocks/data/profileMocks.js
@@ -0,0 +1,16 @@
+export const profileMocks = [
+  {
+    owner: 'oiw223h3qndlksadffl3ksdf',
+    created: new Date('2017-07-01T01:16:43.881Z'),
+    updated: new Date('2017-07-01T01:16:43.881Z'),
+    firstName: 'Larry',
+    lastName: 'Finklestein',
+  },
+  {
+    owner: '9328fhs938hfdsdblkhf98',
+    created: new Date('2017-07-06T22:02:11.504Z'),
+    updated: new Date('2017-07-06T22:02:11.504Z'),
+    firstName: 'Bobby',
+    lastName: 'Johnson',
+  },
+];
--- src/globals/mocks/index.js
+++ src/globals/mocks/index.js
@@ -1,11 +1,13 @@
 import { authorMocks } from './data/authorMocks';
 import { bookBucketMocks } from './data/bookBucketMocks';
 import { bookMocks } from './data/bookMocks';
+import { profileMocks } from './data/profileMocks';
 import { userMocks } from './data/userMocks';
 
 export {
   authorMocks,
   bookBucketMocks,
   bookMocks,
+  profileMocks,
   userMocks,
 };
