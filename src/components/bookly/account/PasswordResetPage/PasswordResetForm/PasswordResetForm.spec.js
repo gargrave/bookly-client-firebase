f79a9900715c32db83e5e477b08999416cc7e585
Gabe Hargrave <gkhargrave@gmail.com>
2018-04-21 18:54:20 -0700

    Add tests

---
+++ src/components/bookly/account/PasswordResetPage/PasswordResetForm/PasswordResetForm.spec.js
@@ -0,0 +1,42 @@
+import { ComponentBuilder } from '../../../../../globals/utils/testHelpers';
+
+import passwordResetModel from '../../../../../models/PasswordReset.model';
+
+import Form from '../../../../common/Form';
+import InputField from '../../../../common/InputField';
+
+import PasswordResetForm from './PasswordResetForm';
+
+const fakeEmail = 'fakeemail@email.com';
+const defaultProps = {
+  disabled: false,
+  email: fakeEmail,
+  errors: passwordResetModel.emptyErrors(),
+  onInputChange: jest.fn(),
+  onSubmit: jest.fn(),
+  submitDisabled: false,
+  topLevelError: '',
+};
+
+const builder = new ComponentBuilder(
+  PasswordResetForm,
+  defaultProps,
+);
+
+describe('PasswordResetForm', () => {
+  let component;
+
+  describe('the basics', () => {
+    test('matches the snapshot', () => {
+      component = builder.shallowGetComponent();
+      expect(component).toMatchSnapshot();
+    });
+
+    test('renders correctly', () => {
+      component = builder.mountComponent();
+      expect(component.find('.bookly-password-reset-form')).toHaveLength(1);
+      expect(component.find(Form)).toHaveLength(1);
+      expect(component.find(InputField)).toHaveLength(1);
+    });
+  });
+});
