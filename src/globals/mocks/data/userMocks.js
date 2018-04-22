d7c55eb21404250dd35fd137098e432097c8d5e3
Gabe Hargrave <gkhargrave@gmail.com>
2018-04-22 12:15:49 -0700

    Update a bit of mock data

--- src/globals/mocks/data/userMocks.js
+++ src/globals/mocks/data/userMocks.js
@@ -1,10 +1,11 @@
 const userMocks = [
   {
-    uid: 'h6EfCE6MrJ5IVf2ay3QsPdX5Jd23',
-    registered: new Date('2017-06-30T19:52:54.644Z'),
-    lastLogin: new Date('2017-07-01T01:11:03.942Z'),
+    displayName: null,
     email: 'mockuser1@gmail.com',
-    verified: true,
+    emailVerified: true,
+    id: 'h6EfCE6MrJ5IVf2ay3QsPdX5Jd23',
+    lastLogin: new Date('2017-07-01T01:11:03.942Z'),
+    registered: new Date('2017-06-30T19:52:54.644Z'),
   },
 ];
 
