341f527b2423d1285bd0f48b668560657ba4bee8
Gabe Hargrave <gkhargrave@gmail.com>
2018-08-02 17:20:21 -0700

    Add better/clean env parsing

--- src/wrappers/firebase/index.js
+++ src/wrappers/firebase/index.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+import { isDevEnv } from '../../globals/env';
 import { devConfig, prodConfig } from './firebaseConfig';
 
 let initialized = false;
@@ -9,11 +10,7 @@ let firebaseAuth;
 let db;
 
 if (!initialized) {
-  const isDev =
-    window.location.hostname.match(/bookly-dev/)
-    || window.location.hostname.match(/localhost/);
-  firebase.initializeApp(isDev ? devConfig : prodConfig);
-
+  firebase.initializeApp(isDevEnv() ? devConfig : prodConfig);
   firebaseAuth = firebase.auth();
   const firestore = firebase.firestore();
   const settings = { timestampsInSnapshots: true };
