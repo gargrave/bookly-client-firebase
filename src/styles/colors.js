4a442965322ad2a2d7bde2737b6f3d8926fe1742
Gabe Hargrave <gkhargrave@gmail.com>
2018-09-24 21:41:31 -0700

    Set up 'colors' emotion helper

---
+++ src/styles/colors.js
@@ -0,0 +1,17 @@
+const error = '#ff4136'
+
+const headerMain = '#d1609c'
+const headerLight = '#e485b7'
+
+const textDefault = '#232323'
+const textMed = '#5d5656'
+const textLight = '#727272'
+
+export default {
+  error,
+  headerMain,
+  headerLight,
+  textDefault,
+  textMed,
+  textLight,
+}
